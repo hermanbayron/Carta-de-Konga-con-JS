e3a0ec30f61c8d9e6ce5568da634857d0f0ee75b
hermanbayron <foforito2001gg@gmail.com>
2022-09-29 20:34:27 +0000

    Carta de Konga con JS

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const palo = ["♦", "♥", "♠", "♣"];
+  const number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let randomPalo = Math.floor(Math.random() * palo.length);
+  console.log(randomPalo);
+  let randomNumero = Math.floor(Math.random() * number.length);
+  console.log(randomNumero);
+
+  let returnPaloTop = (document.getElementById("paloTop").innerHTML =
+    palo[randomPalo]);
+  let returnNumber = (document.getElementById("number").innerHTML =
+    number[randomNumero]);
+  let returnPaloBot = (document.getElementById("paloBot").innerHTML =
+    palo[randomPalo]);
+
+  return {
+    returnPaloTop,
+    returnNumber,
+    returnPaloBot
+  };
 };
